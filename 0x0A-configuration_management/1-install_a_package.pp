d1d7f0b526e9a134a1b0919e2323dbf5f4a29876
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-25 16:08:52 +0000

    Add Configuration management project directory

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# Using Puppet, install flask from pip3
+
+exec {'install flask':
+  command => '/usr/bin/pip3 install python3-flask>=2.1.0'
+  }
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+This directory contians the code files for the **Configuration management** project.
+There are 3 mandatory tasks in the project.
