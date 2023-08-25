7dc2114d63a6bc288926e940411678941339f1c4
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-25 16:24:07 +0000

    Add required comma to end of task 1 command line

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet, install flask from pip3
 
 exec {'install flask':
-  command => '/usr/bin/pip3 install python3-flask==2.1.0'
+  command => '/usr/bin/pip3 install python3-flask==2.1.0,'
   }
