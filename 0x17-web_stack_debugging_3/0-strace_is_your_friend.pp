adca25a71c5e1d641d54448031b1b95c82b17d4a
primeace7 <osaruonamenjeffery@gmail.com>
2023-10-15 20:22:20 +0000

    Fix puppet-lint errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest to fix 500 server error in a wordpress site
 
 exec {'fix wordpress':
-     command => "/bin/sed -i 's/**.phpp/.php/' /var/www/html/wp-settings.php"
+  command => "/bin/sed -i 's/**.phpp/.php/' /var/www/html/wp-settings.php"
 }
