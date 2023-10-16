cdb2f7452b441b577889b688aa5b7582d4e3e865
primeace7 <osaruonamenjeffery@gmail.com>
2023-10-16 13:12:07 +0000

    Refactor task 0 puppet manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,11 @@
-#fix nginx error 24: too many files open by increasing nginx worker_preocesses limit to 1000
-exec {'increase nginx worker_processes':
-          command => "/bin/sed -i 's/^worker_processes[[:space:]]4;$/worker_processes 1000;/' /etc/nginx/nginx.conf"
+#fix nginx error 24: too many files open
+
+$uri = "/bin/sed -i \'s/^ULIMIT=\"-n [[:digit:]]\\{2\\}\"/ULIMIT=\"-n 4090\"/\' /etc/default/nginx"
+exec {'increase nginx worker_processes files limit':
+          command => $uri
 }
 
 exec {'restart nginx':
-  command => '/usr/sbin/service nginx restart'
-  }
+          command => '/usr/sbin/service nginx restart',
+          require => Exec['increase nginx worker_processes files limit']
+}
